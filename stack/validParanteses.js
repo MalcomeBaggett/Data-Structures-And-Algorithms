688535b106b7483b865824b63b6477d0f7750cff
Malcome Baggett <MalcomeBaggett@gmail.com>
2023-10-09 05:57:36 -0400

    added valid parantheses solution

---
+++ stack/validParanteses.js
@@ -0,0 +1,23 @@
+function solution(s) {
+  const map = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+  }
+  const stack = []
+
+  for (const char of s) {
+    const isBracket = char in map
+    if (!isBracket) {
+      stack.push(char)
+      continue
+    }
+    const isEqual = stack[stack.length - 1] === map[char]
+    if (isEqual) {
+      stack.pop()
+      continue
+    }
+    return false
+  }
+  return stack.length === 0
+}
